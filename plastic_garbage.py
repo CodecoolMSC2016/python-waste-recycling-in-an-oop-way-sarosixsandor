b0604895a88c8cb947c490bdfbdf1e93d27a2b14
sanziii <sarosixsandor@gmail.com>
2017-02-27 01:37:56 +0100

    tests running

--- plastic_garbage.py
+++ plastic_garbage.py
@@ -2,4 +2,11 @@ from garbage import Garbage
 
 
 class PlasticGarbage(Garbage):
-    pass
+
+    def __init__(self, name, is_clean):
+        self.name = name
+        self.is_clean = is_clean
+
+    def clean(self):
+        if not self.is_clean:
+            self.is_clean = True
