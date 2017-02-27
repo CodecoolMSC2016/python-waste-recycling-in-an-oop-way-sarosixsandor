c09fe188ca9f5d4059d83b9cfe48b32285dc0261
sanziii <sarosixsandor@gmail.com>
2017-02-27 01:37:42 +0100

    tests running

--- paper_garbage.py
+++ paper_garbage.py
@@ -2,4 +2,11 @@ from garbage import Garbage
 
 
 class PaperGarbage(Garbage):
-    pass
+
+    def __init__(self, name, is_squeezed):
+        self.name = name
+        self.is_squeezed = is_squeezed
+
+    def squeeze(self):
+        if not self.is_squeezed:
+            self.is_squeezed = True
