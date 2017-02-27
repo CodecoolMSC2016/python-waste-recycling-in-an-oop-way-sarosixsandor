d7789df86f53abba4be2a88a40dea28e2c79fc25
sanziii <sarosixsandor@gmail.com>
2017-02-27 01:35:52 +0100

    tests running

--- dustbin.py
+++ dustbin.py
@@ -5,4 +5,28 @@ from dustbin_content_error import DustbinContentError
 
 
 class Dustbin:
-    pass
+
+    def __init__(self, color):
+        self.color = color
+        self.plastic_content = []
+        self.paper_content = []
+        self.house_waste_content = []
+
+    def throw_out_garbage(self, garbage):
+        if (type(garbage) == PaperGarbage) and garbage.is_squeezed:
+            self.paper_content.append(garbage)
+        if (type(garbage) == PlasticGarbage) and garbage.is_clean:
+            self.plastic_content.append(garbage)
+        if type(garbage) == Garbage:
+            self.house_waste_content.append(garbage)
+        if (type(garbage) == str):
+            raise DustbinContentError
+        if (type(garbage) == PlasticGarbage) and not garbage.is_clean:
+            raise DustbinContentError
+        if (type(garbage) == PaperGarbage) and not garbage.is_squeezed:
+            raise DustbinContentError
+
+    def empty_contents(self):
+        self.plastic_content = []
+        self.paper_content = []
+        self.house_waste_content = []
